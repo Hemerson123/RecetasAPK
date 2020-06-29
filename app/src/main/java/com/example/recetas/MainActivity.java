766adcd6fe5711629e7778c5c61e9acf061b33be
Hemerson123 <hemersonvega2001@gmail.com>
2020-06-29 21:21:16 +0200

    Fondo de Pantalla de App

--- app/src/main/java/com/example/recetas/MainActivity.java
+++ app/src/main/java/com/example/recetas/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.recetas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -20,11 +21,12 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setJavaScriptEnabled(true);
         webSettings.setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
 
 
         webView.loadUrl("http://185.254.206.236/");
 //webView.loadUrl("http://185.254.206.236/");
-        webView.setWebViewClient(new WebViewClient());
+// webView.setWebViewClient(new WebViewClient());
 
 
     }
